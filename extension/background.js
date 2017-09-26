40e3936929e2ce16aedc4e548e98fbc817ef0970
rishipuri <rishipuri@live.in>
2017-09-26 09:42:58 +0100

    Use chrome storage to save state

--- extension/background.js
+++ extension/background.js
@@ -2,20 +2,30 @@ chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.query({
     audible: true
   }, function(tabs) {
-    tabs.forEach(function(tab) {
-      if (tab.audible && !tab.mutedInfo.muted) {
-        chrome.tabs.update(tab.id, {
-          muted: true
-        }, function(tab) {
-          console.log('muted ' , tab)
-        })
-      } else {
-        chrome.tabs.update(tab.id, {
-          muted: false
-        }, function(tab) {
-          console.log('unmuted ' , tab)
-        })
-      }
-    })
+    getState(function (state) {
+      tabs.forEach(function(tab) {
+        updateTab(tab, !state.muted);
+      });
+    });
   })
 })
+
+function saveState(state) {
+  chrome.storage.sync.set({
+    muted: state
+  });
+}
+
+function getState(callback) {
+  chrome.storage.sync.get({
+    muted: false
+  }, callback);
+}
+
+function updateTab(tab, mute) {
+  chrome.tabs.update(tab.id, {
+    muted: mute
+  }, function(tab) {
+    saveState(mute);
+  })
+}
